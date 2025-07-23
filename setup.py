55ee45f4a3f89416a96d8c9cb6a60a82be00f753
sovan65 <155409529+sovan65@users.noreply.github.com>
2025-07-23 17:34:48 +0530

    Add script to create directories and empty files from list_of_files with logging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="sovan Banerjee",
+    author_email="sovancse03@gmail.cpm.com",
+    packages=find_packages(),
+    install_requires=[]
+)
